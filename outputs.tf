693f4799302a3740a05506eb94ca09a5b52ecfaf
huayuenh <huayuenh@ie.ibm.com>
2023-04-12 23:15:53 +0100

    ci: enable cra

--- outputs.tf
+++ outputs.tf
@@ -4,37 +4,37 @@
 
 output "compliance_ci_toolchain_id" {
   description = "The ID of the Compliance CI Toolchain"
-  value       = try(module.devsecops_ci_toolchain[0].toolchain_id, "")
+  value       = try(module.devsecops_ci_toolchain[0].toolchain_id, "Not created")
 }
 
 output "compliance_cd_toolchain_id" {
   description = "The ID of the Compliance CD Toolchain"
-  value       = try(module.devsecops_cd_toolchain[0].toolchain_id, "")
+  value       = try(module.devsecops_cd_toolchain[0].toolchain_id, "Not created")
 }
 
 output "compliance_cc_toolchain_id" {
   description = "The ID of the Compliance CC Toolchain"
-  value       = try(module.devsecops_cc_toolchain[0].toolchain_id, "")
+  value       = try(module.devsecops_cc_toolchain[0].toolchain_id, "Not created")
 }
 
 output "evidence_repo_url" {
   description = "The Evidence Repo URL"
-  value       = try(module.devsecops_ci_toolchain[0].evidence_repo_url, "")
+  value       = try(module.devsecops_ci_toolchain[0].evidence_repo_url, var.evidence_repo_url)
 }
 
 output "issues_repo_url" {
   description = "The Issues Repo URL"
-  value       = try(module.devsecops_ci_toolchain[0].issues_repo_url, "")
+  value       = try(module.devsecops_ci_toolchain[0].issues_repo_url, var.issues_repo_url)
 }
 
 output "inventory_repo_url" {
   description = "The Inventory Repo URL"
-  value       = try(module.devsecops_ci_toolchain[0].inventory_repo_url, "")
+  value       = try(module.devsecops_ci_toolchain[0].inventory_repo_url, var.inventory_repo_url)
 }
 
 output "app_repo_url" {
   description = "The App Repo URL"
-  value       = try(module.devsecops_ci_toolchain[0].app_repo_url, "")
+  value       = try(module.devsecops_ci_toolchain[0].app_repo_url, var.cc_app_repo_url)
 }
 
 #############################################################################
