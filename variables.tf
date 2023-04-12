9f26a4cd716c7e77e0079df3b99757c05dd890fd
huayuenh <huayuenh@ie.ibm.com>
2023-04-12 22:34:48 +0100

    ci: add catalog json

--- variables.tf
+++ variables.tf
@@ -1061,7 +1061,7 @@ variable "cd_change_management_repo_git_token_secret_name" {
 
 variable "cd_scc_integration_name" {
   type        = string
-  description = "The name of the SCC integration name."
+  description = "The name of the SCC integration."
   default     = "Security and Compliance"
 }
 
@@ -1502,7 +1502,7 @@ variable "cc_app_repo_git_token_secret_name" {
 
 variable "cc_scc_integration_name" {
   type        = string
-  description = "The name of the SCC integration name."
+  description = "The name of the SCC integration."
   default     = "Security and Compliance"
 }
 
