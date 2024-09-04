cc54a5b407182a0985f1197f742f72956024824a
huayuenh <48723418+huayuenh@users.noreply.github.com>
2024-09-04 17:14:14 +0100

    ci: pin hashicorp versions (#472)

--- version.tf
+++ version.tf
@@ -9,12 +9,12 @@ terraform {
 
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.6.2"
+      version = "= 3.6.2"
     }
 
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.2"
+      version = "= 3.2.2"
     }
   }
 }
