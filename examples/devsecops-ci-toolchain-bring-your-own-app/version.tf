7166908bba54cd5fe296a4f8e7f8d697bbc9096c
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2026-05-22 20:14:16 +0530

    bump provider version to other modules

--- examples/devsecops-ci-toolchain-bring-your-own-app/version.tf
+++ examples/devsecops-ci-toolchain-bring-your-own-app/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.79.2, < 2.0.0"
+      version = ">= 1.79.2, < 3.0.0"
     }
   }
 }
--- prereqs/versions.tf
+++ prereqs/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.82.1, < 2.0.0"
+      version = ">= 1.82.1, < 3.0.0"
     }
 
     external = {
