01748ffc25accfa26eead771c39f0eaa8fdf746b
Eric Jodet <eric_jodet@fr.ibm.com>
2026-06-17 13:28:20 +0200

    feat/board-issue-6636
    save wip

---
+++ examples/ghec-terraform/variables.tf
@@ -0,0 +1,53 @@
+variable "ibmcloud_api_key" {
+  sensitive = true
+}
+
+variable "sm_instance_crn" {
+}
+
+variable "toolchain_name" {
+}
+
+variable "toolchain_region" {
+  default = "us-south"
+}
+
+variable "toolchain_resource_group" {
+  default = "jumpstart"
+}
+
+variable "app_repo_existing_url" {
+  default = ""
+}
+
+variable "repo_git_id" {
+  default = ""
+}
+
+variable "pipeline_config_repo_git_id" {
+  default = "hostedgit"
+}
+
+variable "repo_git_token_secret_crn" {
+  default = ""
+}
+
+variable "app_repo_branch" {
+  default = "main"
+}
+
+variable "inventory_repo_existing_url" {
+  default = ""
+}
+
+variable "issues_repo_existing_url" {
+  default = ""
+}
+
+variable "ghec_git_token_secret_crn" {
+  default = ""
+}
+
+variable "change_management_repo_existing_url" {
+  default = ""
+}
